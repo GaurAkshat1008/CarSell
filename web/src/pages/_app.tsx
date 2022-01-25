11c5503945ee43f3f1e495584f339d9112779c99
Akshat <akshatgauriiitsurat@gmail.com>
2022-01-25 21:03:14 +0530

    first commit

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,19 +1,24 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { createClient, Provider } from "urql";
-import theme from "../theme";
+import { ChakraProvider } from '@chakra-ui/react'
 
-const client = createClient({url: "http://localhost:4000/graphql", fetchOptions: {
-  credentials:'include',
-}})
+import theme from '../theme'
+import { AppProps } from 'next/app'
+import { createClient, Provider } from 'urql'
 
-function MyApp({ Component, pageProps }: any) {
+const client = createClient({
+  url:"http://localhost:4000/graphql",
+  fetchOptions:{
+    credentials:'include'
+  }
+})
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider value={client}>
+    <Provider value={client} >
     <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
     </Provider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
